014c313e084248322dd48250e7f7790cc2761bd7
peculiaruc <peculiarumeh02@gmail.com>
2021-02-08 03:17:24 +0100

    implement TaskAdapter

--- app/src/main/java/com/peculiaruc/notesapp/view/TodoView.java
+++ app/src/main/java/com/peculiaruc/notesapp/view/TodoView.java
@@ -1,4 +1,26 @@
 package com.peculiaruc.notesapp.view;
 
-public class TodoView {
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class TodoView extends ConstraintLayout {
+    public TodoView(@NonNull Context context) {
+        super(context);
+    }
+
+    public TodoView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TodoView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public TodoView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
 }
